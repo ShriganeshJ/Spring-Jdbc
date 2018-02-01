d9b76f498a9c37944bde639dc41d74b786baf99e
Mr.Shriganesh <Mr.Shriganesh@DESKTOP-PSE05UQ>
2018-02-01 20:17:08 +0530

    Add DAO class

---
+++ JdbcTempletUsingServerManagedConnectPool/src/main/java/com/org/dao/EmployeeDAOImpl.java
@@ -0,0 +1,25 @@
+package com.org.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+	//Configure The JDBC Templet
+	JdbcTemplate jt;
+	//create one list
+	List<Map<String,Object>> list=null;
+	@Override
+	public List<Map<String, Object>> list(String condition) {
+		
+		list=jt.queryForList("SELECT EID,ENAME,JOB,SAL FROM EMPLOYEE WHERE JOB IN"+condition);
+		return list;
+	}
+
+	public void setJt(JdbcTemplate jt) {
+		this.jt = jt;
+	}
+
+}
