ebdd8480e6840051573f1278723762c7cbf149d6
Mr.Shriganesh <Mr.Shriganesh@DESKTOP-PSE05UQ>
2018-02-01 19:53:17 +0530

    Add basic Setting

--- JdbcTempletUsingServerManagedConnectPool/src/main/java/com/org/controller/EmployeeController.java
+++ JdbcTempletUsingServerManagedConnectPool/src/main/java/com/org/controller/EmployeeController.java
@@ -1,37 +1,26 @@
 package com.org.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-/**
- * Servlet implementation class EmployeeController
- */
 public class EmployeeController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+	
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public EmployeeController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+   	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+   		PrintWriter pw=null;
+   		//General Setting
+   		response.setContentType("text/html");
+   		pw=response.getWriter();
+		}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
