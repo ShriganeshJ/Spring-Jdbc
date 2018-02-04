d41e1820724be32041af22d74588fe47d0f9c8fe
Mr.Shriganesh <Mr.Shriganesh@DESKTOP-PSE05UQ>
2018-02-04 17:07:52 +0530

    commit

---
+++ JdbcTempletUsingAnnotation-xml/src/main/java/com/org/test/Test.java
@@ -0,0 +1,30 @@
+package com.org.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.org.servie.EmployeeServiceImpl;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx=null;
+		EmployeeServiceImpl es=null;
+		
+		ctx=new ClassPathXmlApplicationContext("com/org/cfg/applicationContext.xml");
+		
+		es=ctx.getBean(EmployeeServiceImpl.class);
+		//System.out.println(es.ServiceCount());
+		
+		//System.out.println(es.getMapDetails(10));
+		
+		//System.out.println(es.getAlDetails(11));
+		
+		System.out.println(es.update(12,70000));
+		
+		
+		
+
+	}
+
+}
